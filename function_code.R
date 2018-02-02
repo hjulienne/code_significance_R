b1d788c5758f9094a98b2928a9c5e1aaa6066fd6
EvenStar69 <35998015+EvenStar69@users.noreply.github.com>
2018-02-02 12:00:09 +0100

    Update function_code.R

--- function_code.R
+++ function_code.R
@@ -1,4 +1,14 @@
-significant_bar <- function(){
+##### arguments : #####
+
+# REQUIRED ARGUMENTS
+# (1) the plot
+# (2) the bars between which the significance bar is going to be added
+# could be by number (starting from 1 from left to right) but probably better if it's by name ?
+# (3) characters/stars
+
+# FALCULTATIVE / COSMETIC ARGUMENTS
+# (4) 
+
+significant_bar <- function(plot = , ){
   
 }
-
